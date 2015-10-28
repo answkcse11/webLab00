dce5a23c4707960e63474835ff30ea2cae7c00c3
Ji younghyun <jlh7020@gmail.com>
2015-10-28 18:33:55 +0900

    2015/10/28

--- db/ex6(world).sql
+++ db/ex6(world).sql
@@ -1,9 +1,9 @@
 -- Ex 6. --
-select countries.name, languages.languages from countries, languages
-where countries.code=languages.country_code and countries.independent_year=1948;
+select countries.name, countries.independence_year, languages.language from countries, languages
+where countries.code=languages.country_code and countries.independence_year=1948;
 
 select countries.name from countries, languages
-where countrires.code=languages.country_code and
+where countries.code=languages.country_code and
 ((languages.language='French' and languages.official='T') or
 (languages.language='English' and languages.official='T'));
 
@@ -14,8 +14,13 @@ group by language order by count(country_code) desc limit 5;
 
 select capital from countries, languages
 where countries.code=languages.country_code and
-(languages.language='Korean' or languages.language='English');
+(languages.language='Korean' and languages.language='English');
 
-select capital from contries, languages
+select capital from countries, languages
 where countries.code=languages.country_code and
-(languages.official='T' and languages.percentage>20 and languages.percentage<50)
+(languages.official='T' and languages.percentage>20 and languages.percentage<50);
+
+select sum(surface_area) from countries,
+	(select country_code, avg(population) from cities group by country_code
+		order by avg(population) desc limit 5) as NEWTABLE
+where NEWTABLE.country_code=countries.code;
